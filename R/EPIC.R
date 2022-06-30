e8c551e839ae78239937979a48bed756f42fcc42
Raúl Sanz <74537603+raulsanzr@users.noreply.github.com>
2022-06-30 11:12:05 +0200

    Update and rename MethylationEPIC.R to EPIC.R

--- R/EPIC.R
+++ R/EPIC.R
@@ -1,65 +1,48 @@
 #
-# Authors: Raúl Sanz, Angelika Merkel                  
+# Author: Raul Sanz               
+# Description: Pipeline to analyze human methylation array data from Illumina's Infinium MethylationEPIC array.
 #
-# Description: Workflow to analyze human methylation array data from Illumina's Infinium MethylationEPIC BeadChip.
-#
 
-# PACKAGES INSTALLATION
+# DEPENDENCIES
 
-# BiocManager::install("minfi")
-# BiocManager::install("limma")
-# BiocManager::install("DMRcate")
-# remotes::install_github("markgene/maxprobes")
-# install.packages("readxl")
-# install.packages("dplyr")
-# install.packages("ggplot2")
-# install.packages("ggrepel")
-# install.packages("ggfortify")
-# install.packages("gplots")
-# install.packages("RColorBrewer")
-# devtools::install_github("raulsanzr/methtools")
-
-# REQUIRED PACKAGES
-
-library(minfi)
-library(limma)
-library(DMRcate)
-library(maxprobes)
-library(readxl)
-library(dplyr)
-library(ggplot2)
-library(ggrepel)
-library(ggfortify)
-library(gplots)
-library(RColorBrewer)
-library(methtools)
+library(minfi) # BiocManager::install("minfi")
+library(limma) # BiocManager::install("limma")
+library(DMRcate) # BiocManager::install("DMRcate")
+library(maxprobes) # remotes::install_github("markgene/maxprobes")
+library(readxl) # install.packages("readxl")
+library(dplyr) # install.packages("dplyr")
+library(ggplot2) # install.packages("ggplot2")
+library(ggrepel) # install.packages("ggrepel")
+library(ggfortify) # install.packages("ggfortify")
+library(gplots) # install.packages("gplots")
+library(RColorBrewer) # install.packages("RColorBrewer")
 
 # LOADING THE DATA
 
-# reading all the methylation array files (.idat) from a folder
-rgSet <- read.metharray.exp("data/human/")
+# reading all the DNA methylation array files (idat) from a folder
+rgSet <- read.metharray.exp("data/")
 
-# reading the metadata file
-metadata <- as.data.frame(read_excel("data/human/PIK3CA_samples_SC.xlsx"))
+# reading the metadata
+metadata <- as.data.frame(read_excel("data/metadata.xlsx"))
 names(metadata)[1:10] <- c("Sample", "Organism", "Tissue", "Type", "Condition", "Preservation", "DNA_quantity", 
                            "EPIC_ID", "EPIC_position", "EPIC_barcode")
 
-## renaming some columns to adapt the metadata
+## formatting the metadata
 metadata$CellType <- paste(metadata$CellType1, metadata$CellType2, sep="_" )
 metadata$CellType <- gsub("thelial","", gsub("_NA","", as.character(metadata$CellType)))
 metadata$Condition <- gsub("pik3ca ","", as.character(metadata$Condition))
 metadata$Type <- gsub("Vascular malformation", "Mutation", metadata$Type)
-metadata <- metadata[,-c(2, 3, 6, 8, 9, 12, 13, 14)]
+metadata$Group <- paste0(metadata$Type,"_", metadata$CellType)
 
 # QUALITY CONTROL
 
-# Generating the quality control report for the raw data 
-qcReport(rgSet, pdf="results/human/qcReport.pdf", sampGroups=metadata$Condition, sampNames=metadata$Sample)
+# generating the QC report
+qcReport(rgSet, pdf="results/qcReport.pdf", sampGroups=metadata$Condition, sampNames=metadata$Sample)
 
 # detection p-values
 p_values <- detectionP(rgSet, type = "m+u")
 
-# mean detection p-values
+## mean detection p-values
 mean_p <- data.frame(p_values=colMeans(p_values), Sample=metadata$Sample)
 
 ## plotting the resulting mean detection p-values
@@ -73,7 +56,8 @@ ggplot(mean_p, aes(x=Sample, y=p_values, fill=p_values))+
 
 # PREPROCESSING
 
-mSet <- preprocessNoob(rgSet)
+# ssNoob normalization
+mSet <- preprocessNoob(rgSet) 
 
 # removing low-quality probes by detection p-value
 keep <- rowSums(p_values<0.01)==ncol(mSet)
@@ -87,7 +71,7 @@ xreactive_probes <- xreactive_probes(array_type="EPIC")
 keep <- !(featureNames(gmSet) %in% xreactive_probes)
 gmSet <- gmSet[keep,]
 
-# removing sex chromosomes
+# removing probes mapping sex chromosomes
 ann <- getAnnotation(IlluminaHumanMethylationEPICanno.ilm10b4.hg19)
 keep <- !(featureNames(gmSet) %in% ann$Name[ann$chr %in% c("chrX","chrY")])
 gmSet <- gmSet[keep,]
@@ -95,14 +79,13 @@ gmSet <- gmSet[keep,]
 # obtaining the beta values
 beta_values <- getBeta(gmSet)
 colnames(beta_values) <- metadata$Sample
-head(beta_values)
 
 # PRINCIPAL COMPONENT ANALYSIS
 
-# selecting the top 100 most variable CpG's to reduce the number of inputs
+# selecting the top 100 most variable CpG sites
 sdv <- apply(beta_values, 1, sd)
-keep <- names(head(sort(sdv,decreasing=T), 100))
-beta_top100 <- beta_values[keep,]
+top100 <- names(head(sort(sdv,decreasing=T), 100))
+beta_top100 <- beta_values[top100,]
 
 # PCA on the top 100 sites
 pca_res <- prcomp(t(beta_top100), scale=T, center=T)
@@ -114,30 +97,20 @@ autoplot(pca_res, x=1, y=2, data=metadata, colour="Condition")+
   xlim(c(-0.5,0.3))+
   theme_bw()
 
-## plotting PC1 and PC2 by cell type
-autoplot(pca_res, x=1, y=2,data=metadata, colour="CellType", shape = "Type")+
-  geom_text_repel(aes(label=Sample, color=CellType),hjust=-0.2, vjust=0, show.legend=F, size=3.5)+
-  scale_color_brewer(palette = "Set1")+ 
-  labs(colour="Cell Type") + xlim(c(-0.5,0.3))+
-  theme_bw()
-
 # DIFFERENTIAL METHYLATION ANALYSIS
 
-# differentially methylated positions (DMPs)
-## building the design matrix
-mutation <- factor(metadata$Condition)
-designMat <- model.matrix(~0+mutation, data=metadata)
-colnames(designMat) <- levels(mutation)
+## DMPs
 
-## building the contrast matrix
-contMat <- makeContrasts(Control-E542K, 
-                         Control-E545K, 
-                         Control-H1047R, 
-                         E542K-E545K, 
-                         E542K-H1047R, 
-                         H1047R-E545K, levels=designMat)
+# design matrix
+designMat <- model.matrix(~0+Group, data=metadata)
+colnames(designMat) <- gsub("group","",colnames(designMat))
 
-## fitting linear model with limma
+# contrast matrix
+contMat <- makeContrasts(Normal_blood-Normal_lymphatic,
+                         Mutation_blood-Mutation_lymphatic,
+                         levels=designMat)
+
+# fitting linear model
 fit <- lmFit(beta_values, designMat)
 fit2 <- contrasts.fit(fit, contMat)
 fit2 <- eBayes(fit2)
@@ -146,40 +119,37 @@ DMP_summary <- summary(decideTests(fit2, p.value=0.01))
 rownames(DMP_summary) <- c("Hyper", "NotSig", "Hypo")
 colnames(DMP_summary) <- gsub(" - ","_", colnames(DMP_summary))
 
-## annotating the DMPs
-contrasts <- colnames(contMat)
-### matching beta values with annotation by the probe names
-annEPICSub <- ann.cpg(cpgs=beta_values, array="EPIC", what="short")
-
-DMP.list <- list() # list to store the DMPs
-cg <- list() # list to store the names of significant probes
+# annotating the DMPs
+## matching beta values with annotation by the probe names
+annEPICSub <- ann[match(rownames(beta_values),ann$Name), c(1:4,12:19,22:ncol(ann))]
 
 DMP.list <- data.frame() # df to store the DMPs
 cg <- list() # list to store the names of significant probes
 
-for (i in 1:length(contrasts)){
+contrasts <- colnames(contMat)
+for (i in 1:length(contrasts)){ # for every comparison...
   # extract the significant DMPs and annotate
   DMP <- topTable(fit2, num=Inf, coef=i, genelist=annEPICSub, p.value=0.01)
   DMP$Contrast <- contrasts[i]
-  # saving the results
   DMP.list <- rbind(DMP.list, DMP)
   cg[[i]] <- row.names(DMP)
 }
 
-# write.csv(DMP.list, "results/human/DMP_list.csv")
+## saving the results
+write.csv(DMP.list, "results/DMP_list.csv")
 
-### merging all the DMP's
+## merging all the DMPs
 DMP_ann <- DMP.list
 
-### classifying DMP's according to its change in methylation
+## classifying DMPs according to its change in methylation
 DMP_ann$Type <- "Hypermethylated"
 DMP_ann$Type[which(DMP_ann$logFC > 0)] <- "Hypomethylated"
 
-### gene feature annotation
+## gene feature annotation
 DMP_ann$UCSC_RefGene_Group[which(DMP_ann$UCSC_RefGene_Group == "")] <- "."
 DMP_ann$UCSC_RefGene_Group_short <- unlist(lapply(strsplit(DMP_ann$UCSC_RefGene_Group, ";"),'[[', 1))
 
-## barplot relative to CpG Islands
+## DMPs relative to the nearest CGI
 DMP_annCGI <- DMP_ann[, c("Contrast", "Relation_to_Island", "Type")]
 
 ggplot(DMP_annCGI, aes(Contrast, fill=Relation_to_Island))+
@@ -190,7 +160,7 @@ ggplot(DMP_annCGI, aes(Contrast, fill=Relation_to_Island))+
   ylab("Number of DMPss")+ 
   xlab("")
 
-## barplot relative to genetic features
+## DMPs relative to gene elements
 DMP_Gene_Group <- DMP_ann[,c("Contrast","UCSC_RefGene_Group_short", "Type")]
 
 ggplot(DMP_Gene_Group, aes(Contrast, fill=UCSC_RefGene_Group_short))+
@@ -203,36 +173,30 @@ ggplot(DMP_Gene_Group, aes(Contrast, fill=UCSC_RefGene_Group_short))+
   xlab("")+ 
   labs(fill="UCSC_RefGene")
 
-## heatmap
+## DMPs heatmap
 DMP_beta <- data.frame(beta_values[unlist(cg), ])
 colnames(DMP_beta) <- paste0(metadata$Sample,"_",metadata$Condition)
-
 colors <- colorRampPalette(c("royalblue", "white", "red"))(n=100) # range of colors
 heatmap.2(as.matrix(t(unique(DMP_beta))), trace="none", density.inf="none", margins=c(7,10), col=colors, cexRow = 1, lwid = c(5,15), lhei = c(5,15))
 
-# differentially methylated regions (DMRs)
-## finding and annotating the DMPs
-DMR.list <- data.frame()
-for(i in 1:length(contrasts)){
-  # annotating the CpG's for each contrast + finding significant CpG's
+## DMRs
+
+DMR.list <- data.frame() # df to store the DMRs
+
+for(i in 1:length(contrasts)){ # for every comparison...
+  # annotating and finding significant CpG sites
   myAnnotation <- cpg.annotate(object=beta_values, datatype="array", what="Beta", analysis.type="differential", design=designMat, contrasts=T, 
                                cont.matrix=contMat, coef=gsub("_", " - ", as.character(contrasts[i])), arraytype="EPIC")
   
-  if(sum(myAnnotation@ranges@elementMetadata@listData$is.sig)!=0){ # if there are significant CpG's
-    # test for DMRs
+  if(sum(myAnnotation@ranges@elementMetadata@listData$is.sig)!=0){ # if there are significant sites...
+    # search for DMRs
     DMR_raw <- dmrcate(myAnnotation, lambda=1000, C=2)
-    # extract genomic ranges
+    # extract the genomic ranges
     results.ranges <- extractRanges(DMR_raw)
-    # saving the results
     DMR <- data.frame(Contrast=contrasts[i], results.ranges)
     DMR.list <- rbind(DMR.list, DMR)
   }
 }
-# write.csv(DMR.list, "results/human/DMR_list.csv")
 
-## plotting the DMRs
-CpGs <- gmSet@rowRanges
-values(CpGs) <- beta_values
-
-meth.plot(genome="hg19", chr="chr11", start=27015473, end=27015991, sites=CpGs, regions=DMR.list, group=metadata$Condition)
-
+## saving the results
+write.csv(DMR.list, "results/DMR_list.csv")
